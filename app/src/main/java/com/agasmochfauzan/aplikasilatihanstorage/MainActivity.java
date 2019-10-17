49621e5f879cabfa661026cc2ea70084f9590d0a
agasmoch <agasm5@gmail.com>
2019-10-17 15:00:39 +0700

    Menyimpan file ke Handphone finish

--- app/src/main/java/com/agasmochfauzan/aplikasilatihanstorage/MainActivity.java
+++ app/src/main/java/com/agasmochfauzan/aplikasilatihanstorage/MainActivity.java
@@ -7,8 +7,11 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
     public static  final String FILENAME= "namefile.txt";
@@ -31,7 +34,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         deleteFile.setOnClickListener(this);
     }
 
-    void buatfile(){
+    void buatFile(){
         String isiFile = "Coba isi data file text";
         File file = new File(getFilesDir(),FILENAME);
 
@@ -39,15 +42,86 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         try{
             file.createNewFile();
             outputStream = new FileOutputStream(file,true);//
-            outputStream.write(isiFile.getBytes());//Input text dengan Tipe data Byte(Tadinya String)
+            outputStream.write(isiFile.getBytes());//Input text dengan Konversi Tipe Data(String ke Byte)
             outputStream.flush();//Bersihkan Text Yang Diisi
             outputStream.close();//Tutup text yang diisi
         } catch (Exception e){
             e.printStackTrace();
         }
     }
+    void ubahFile(){
+        String ubah = "Update Isi Data File Text";
+        File file = new File(getFilesDir(),FILENAME);
+
+        FileOutputStream outputStream =null;
+        try {
+            file.createNewFile();
+            outputStream = new FileOutputStream(file,false);
+            outputStream.write(ubah.getBytes());//Input text dengan Konversi Tipe Data(String ke Byte)
+            outputStream.flush();//Bersihkan Text Yang Diisi
+            outputStream.close();//Tutup text yang diisi
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    void bacaFile(){
+        File sdcard = getFilesDir();
+        File file = new File(sdcard,FILENAME);
+
+        if(file.exists()){ //Mengecek Isi File
+            StringBuilder text = new StringBuilder();//Penampung dan memibikin object "StringBuilder"
+
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(file));
+
+                String line=br.readLine();
+
+                while (line != null) //Line mengunalang jika tidak null
+                {
+                    text.append(line);
+                    line=br.readLine();//Melakukan Pembacaan
+                }
+                br.close();//Tutup text yang diisi
+            }catch (IOException e)
+            {
+                System.out.println("Error"+e.getMessage());
+            }
+            textbaca.setText(text.toString());
+        }
+    }
+
+    void deleteFile(){
+//        File sdcard = getFilesDir();
+        File file = new File(getFilesDir(),FILENAME);
+
+        if (file.exists())//Jika file ada
+        {
+            file.delete();//{Perintah Hapus
+        }
+    }
+
+    public void jalakanPerintah(int id)//Membutuhkan varibel id yang mana yang dijalakan
+    {switch (id){
+        case R.id.buttonBuatFile:
+            buatFile();
+            break;
+        case R.id.buttonBacaFile:
+            bacaFile();
+            break;
+        case R.id.buttonHapusFile:
+            deleteFile();
+            break;
+        case R.id.buttonUbahFile:
+            ubahFile();
+            break;
+    }
+
+    }
     @Override
     public void onClick(View view) {
+        jalakanPerintah (view.getId());
 
     }
 }
